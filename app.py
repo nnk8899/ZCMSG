9af410bc9fc4ab5377d1aa6a56505f94531afa55
XUEHUIYIN <xuehuiyin@sh-zcamc.com>
2018-11-08 15:20:23 +0800

    set "checkAll"&"clearAll"

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+from wtforms import BooleanField, StringField, PasswordField, SelectField, RadioField, SubmitField, IntegerField, TextAreaField, validators
+from wtforms.validators import Length, DataRequired
+from flask_wtf import Form,FlaskForm
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
+from flask_admin.form import widgets
+
 import os
 import pymssql
 import sys_logger
 
+
+class addForm(Form):
+    add_num = IntegerField(u'*模板编号',validators = [DataRequired()])
+    add_theme = StringField(u'*模板主题',validators = [DataRequired()])
+    add_contains = StringField(u'*模板内容', validators=[DataRequired()])
+    add_details = StringField(u'备注')
+    add_submit = SubmitField(u'提交')
+
+
+def link_sql():
+    host = 'LAPTOP-GPKFSA00'
+    user = 'sa'
+    password = 'xyt555'
+    database = 'msg'
+    conn = pymssql.connect(host, user, password, database, charset='cp936')
+    return conn
+
+
+def to_standard(var):
+	standard_var = "'"+str(var)+"'"
+	return standard_var
+
+
+
+
+
 app = Flask(__name__)
 logger = sys_logger.get_logger(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -32,13 +66,27 @@ def menu_bar():
 
 @app.route('/prototypeEditor', methods=['GET', 'POST'])
 def prototype_editor():
-    conn = pymssql.connect(host='LAPTOP-GPKFSA00', user='sa', password='xyt555', database='msg', charset='cp936')
+    conn = link_sql()
     cur = conn.cursor()
     sql = "select * from [dbo].[prototype]"
     cur.execute(sql)
     u = cur.fetchall()
     conn.close()
     return render_template('prototypeEditor.html', u=u)
+
+
+@app.route('/addProto', methods=['GET', 'POST'])
+def add_prototype():
+    form = addForm(request.form)
+    if request.method == 'POST' and form.validate():
+        conn = link_sql()
+        cursor1 = conn.cursor()
+        sql1 = 'insert into [dbo].[prototype] values(' + form.add_num.data + ',' + to_standard(form.add_theme.data) + ',' + to_standard(form.add_contains.data) + ',' + to_standard(form.add_details.data) + ')'
+        cursor1.execute(sql1)
+        conn.commit()
+        conn.close()
+        return redirect('http://localhost:9000/logout')
+    return render_template('addProto.html', form=form)
 
 
 @app.route("/logout")

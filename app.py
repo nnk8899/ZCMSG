402aa201b5e53e60e990a203313505cb007940a0
XUEHUIYIN <xuehuiyin@sh-zcamc.com>
2018-11-09 14:48:01 +0800

    add "editProto" module & fix "addProto" module

--- app.py
+++ app.py
@@ -11,12 +11,19 @@ import pymssql
 import sys_logger
 
 
-class addForm(Form):
+class addForm(FlaskForm):
     add_num = IntegerField(u'*模板编号',validators = [DataRequired()])
     add_theme = StringField(u'*模板主题',validators = [DataRequired()])
-    add_contains = StringField(u'*模板内容', validators=[DataRequired()])
-    add_details = StringField(u'备注')
+    add_contains = TextAreaField(u'*模板内容', validators=[DataRequired()])
+    add_details = TextAreaField	(u'备注')
     add_submit = SubmitField(u'提交')
+
+
+class editForm(FlaskForm):
+    edit_theme = StringField(u'*模板主题',validators = [DataRequired()], default="XX")
+    edit_contains = TextAreaField(u'*模板内容', validators=[DataRequired()])
+    edit_details = TextAreaField	(u'备注')
+    edit_submit = SubmitField(u'提交')
 
 
 def link_sql():
@@ -39,6 +46,7 @@ def to_standard(var):
 app = Flask(__name__)
 logger = sys_logger.get_logger(__name__)
 bootstrap = Bootstrap(app)
+
 
 
 @app.route('/')
@@ -68,7 +76,7 @@ def menu_bar():
 def prototype_editor():
     conn = link_sql()
     cur = conn.cursor()
-    sql = "select * from [dbo].[prototype]"
+    sql = "select * from [dbo].[prototype] order by num asc"
     cur.execute(sql)
     u = cur.fetchall()
     conn.close()
@@ -77,16 +85,47 @@ def prototype_editor():
 
 @app.route('/addProto', methods=['GET', 'POST'])
 def add_prototype():
+    CSRF_ENABLED = True
+    app.config["SECRET_KEY"] = "12345678"
     form = addForm(request.form)
     if request.method == 'POST' and form.validate():
         conn = link_sql()
         cursor1 = conn.cursor()
-        sql1 = 'insert into [dbo].[prototype] values(' + form.add_num.data + ',' + to_standard(form.add_theme.data) + ',' + to_standard(form.add_contains.data) + ',' + to_standard(form.add_details.data) + ')'
+        sql1 = 'insert into [dbo].[prototype] values(' + to_standard(form.add_num.data) + ',' + to_standard(form.add_theme.data) + ',' + to_standard(form.add_contains.data) + ',' + to_standard(form.add_details.data) + ')'
         cursor1.execute(sql1)
         conn.commit()
         conn.close()
-        return redirect('http://localhost:9000/logout')
+        return redirect('http://localhost:9000/prototypeEditor')
     return render_template('addProto.html', form=form)
+
+
+@app.route('/editProto/<id>', methods=['GET','POST'])
+def editProto(id):
+    conn = link_sql()
+    cur = conn.cursor()
+    sql = "select * from [dbo].[prototype] where num=" + to_standard(id)
+    cur.execute(sql)
+    l = cur.fetchall()
+    conn.close()
+    CSRF_ENABLED = True
+    app.config["SECRET_KEY"] = "87654321"
+    form = editForm(request.form)
+    if request.method == 'POST' and form.validate():
+        curr_theme = form.edit_theme.data
+        curr_contains = form.edit_contains.data
+        curr_details = form.edit_details.data
+        conn = link_sql()
+        cursor2 = conn.cursor()
+        sql2 = 'update [dbo].[prototype] set theme=' + to_standard(curr_theme) + ',contain=' + to_standard(curr_contains) + ',details=' + to_standard(curr_details) + 'where num = ' + to_standard(id)
+        cursor2.execute(sql2)
+        conn.commit()
+        conn.close()
+        return redirect('http://localhost:9000/prototypeEditor')
+    form.edit_theme.data = l[0][1]
+    form.edit_contains.data = l[0][2]
+    form.edit_details.data = l[0][3]
+    return render_template('editProto.html', form=form, id=id)
+
 
 
 @app.route("/logout")
